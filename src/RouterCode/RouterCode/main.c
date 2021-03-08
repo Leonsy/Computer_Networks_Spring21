ed033224359e1e0a0c9dc82eaf2526d38aacffc1
Leonsy <leonsy5222@gmail.com>
2021-03-07 22:38:53 -0500

    Init

---
+++ src/RouterCode/RouterCode/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  RouterCode
+//
+//  Created by 朴思扬 on 7/03/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
